b81275820170c4b80867b651f6bf204e59534ec4
yonnie <yonathanmichael63@gmail.com>
2021-04-12 19:30:25 -0400

    NodeServer

---
+++ services/quiz-service.js
@@ -0,0 +1,10 @@
+let quizzes = require("./quizzes.json");
+
+const findAllQuizzes = () => quizzes;
+
+const findQuizById = (quizId) => quizzes.find((quiz) => quiz._id === quizId);
+
+module.exports = {
+	findAllQuizzes,
+	findQuizById,
+};
